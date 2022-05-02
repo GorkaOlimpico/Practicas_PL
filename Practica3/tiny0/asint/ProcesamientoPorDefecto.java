13ba54ad4865edeea28e02aa894116ef5c584e14
Gorka Silva <gorka.silva@hotmail.com>
2022-05-02 22:21:39 +0200

    update

---
+++ Practica3/tiny0/asint/ProcesamientoPorDefecto.java
@@ -0,0 +1,203 @@
+package asint;
+
+import asint.TinyASint.And;
+
+import asint.TinyASint.Beq;
+import asint.TinyASint.Bge;
+import asint.TinyASint.Bgt;
+import asint.TinyASint.Ble;
+import asint.TinyASint.Blt;
+import asint.TinyASint.Bne;
+import asint.TinyASint.Dec;
+import asint.TinyASint.Div;
+import asint.TinyASint.Id;
+import asint.TinyASint.Inst;
+import asint.TinyASint.Lista_dec_muchas;
+import asint.TinyASint.Lista_dec_una;
+import asint.TinyASint.Lista_inst_muchas;
+import asint.TinyASint.Lista_inst_una;
+import asint.TinyASint.Menos_unario;
+import asint.TinyASint.Mul;
+import asint.TinyASint.Not;
+import asint.TinyASint.Num;
+import asint.TinyASint.Or;
+import asint.TinyASint.Prog;
+import asint.TinyASint.R_false;
+import asint.TinyASint.R_true;
+import asint.TinyASint.Resta;
+import asint.TinyASint.Suma;
+import asint.TinyASint.T_bool;
+import asint.TinyASint.T_int;
+import asint.TinyASint.T_real;
+
+public class ProcesamientoPorDefecto implements Procesamiento {
+
+	@Override
+	public void procesa(Suma exp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void procesa(Resta exp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void procesa(Mul exp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void procesa(Div exp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void procesa(Id exp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void procesa(Num exp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void procesa(Dec dec) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void procesa(Menos_unario menos_unario) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void procesa(Not not) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void procesa(Beq beq) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void procesa(Bne bne) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void procesa(Bge bge) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void procesa(Ble ble) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void procesa(Blt blt) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void procesa(Bgt bgt) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void procesa(And and) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void procesa(Or or) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void procesa(R_true r_true) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void procesa(R_false r_false) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void procesa(Lista_inst_una lista_inst_una) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void procesa(Lista_inst_muchas lista_inst_muchas) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void procesa(Prog prog) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void procesa(Inst inst) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void procesa(Lista_dec_una lista_dec_una) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void procesa(Lista_dec_muchas lista_dec_muchas) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void procesa(T_int t_int) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void procesa(T_real t_real) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void procesa(T_bool t_bool) {
+		// TODO Auto-generated method stub
+		
+	}
+   
+}
